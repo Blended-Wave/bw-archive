8c41a283f30c8fca4a8c38fbd495dd662167be8c
seul <angel3030303@gmail.com>
2023-09-22 16:03:01 +0900

    [#25] bug : 글씨체 관련 코드 이동
    - _app.tsx 파일에 link 태그를 통해서 글씨체 관련 코드를 넣을 때 경고문이 나옴
    - _document.tsx에 코드를 이동하여 해결

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Noto+Sans+KR&display=swap"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
