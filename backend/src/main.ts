7ca120817e55977c7196b3e6c3a705c7307e525b
k.seonwoo <sunwoo005@naver.com>
2023-12-11 22:44:14 +0900

    Refactor: "main.ts cors 설정 간편하게 수정"

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,21 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('api');	// <- global prefix
 
   
-  // CORS 설정 옵션 정의
-  const corsOptions: CorsOptions = {
-    origin: true, // 모든 origin을 허용하거나, 특정 origin을 지정
-    methods: 'GET, PUT, POST, DELETE', // 허용할 HTTP 메서드
-    credentials: true, // 쿠키 등 인증 정보를 전송할 수 있도록 허용
-  };
-
-  // CORS 미들웨어를 적용
-  app.enableCors(corsOptions);
 
   await app.listen(4000);
 }
