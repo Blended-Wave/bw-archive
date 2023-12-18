3936b4576cd986d6de6e3470e68f4d3dbdd724da
k.seonwoo <sunwoo005@naver.com>
2023-12-18 17:18:41 +0900

    Feat: "환경변수 도입"

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminWorkModule } from './admin/work/admin.work.module';
 import { AdminUserModule } from './admin/user/admin.user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AdminUserModule, AdminWorkModule],
+  imports: [AdminUserModule, AdminWorkModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
