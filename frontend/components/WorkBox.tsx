f43a54860b61e9e6300c8daa52b20caed3dfc53f
0129jonsu <0129jonsu@gmail.comgit config --global user.email 0129jonsu@gmail.comgit clone it config --global user.email 0129jonsu@gmail.com>
2024-04-07 00:03:41 +0900

    Feat : work page mouse hover시 이펙트 추가

--- frontend/components/WorkBox.tsx
+++ frontend/components/WorkBox.tsx
@@ -5,6 +5,18 @@ export default function WorkBox() {
     return (
         <div className={styles.work_container}>
             <img src="https://picsum.photos/331/331" />
+            <Link className={styles.detail} href="/artist/1">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="36"
+                        height="36"
+                        viewBox="0 0 36 36"
+                        fill="none"
+                    >
+                        <path d="M18 -3.05176e-05V36" stroke="white" stroke-width="3" />
+                        <path d="M-1.52588e-05 18H36" stroke="white" stroke-width="3" />
+                    </svg>
+                </Link>
         </div>
     )
 }
