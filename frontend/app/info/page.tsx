037f955b125b3f4d058693668962ec1c7e666fff
JunSuKim <12191747@inha.edu>
2024-04-30 12:33:56 +0900

    Feat : Info page 추가

--- frontend/app/info/page.tsx
+++ frontend/app/info/page.tsx
@@ -1,7 +1,25 @@
+import styles from '@/styles/Info.module.css';
+
 export default function Info() {
     return (
         <main>
-            <h1>Info</h1>
+            <div className={styles.info_container}>
+                <h1> INFORMATION</h1>
+                <img src="main_logo.svg" alt="logo" className={styles.logo_img} />
+                <img src="background.svg" className={styles.background_img} />
+                <p className={styles.p1}>
+                    The authentic label that presents a new paradigm based on its originalityan brings beloved music upon diverse style and taste.
+                </p>
+                <p className={styles.p2}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <p className={styles.p3}>CONTACT</p>
+                <div className={styles.contact_container}>
+                    <img src="twitter.svg" />
+                    <img src="instagram.svg" />
+                    <img src="mail.svg" />
+                </div>
+            </div>
         </main>
     );
 }
