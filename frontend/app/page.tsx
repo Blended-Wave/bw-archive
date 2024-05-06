bf7f564467b8ecd00862de09dc575843253aec96
JunsuKim <0129jonsu@gmail.com>
2024-05-06 17:12:56 +0900

    Feat : main page 영상 교체, 비율 교체

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,13 +1,14 @@
 export default function Home() {
     return (
         <>
-            <div>
-                <iframe
-                    className="screen"
-                    src="https://www.youtube.com/embed/cbuZfY2S2UQ?start=0&mute=1&autoplay=1&rel=0&controls=0&showinfo=0"
-                    title="YouTube video player"
-                    allow="autoplay; encrypted-media;"
-                ></iframe>
+            <div className="screen_container">
+                <iframe 
+                className="screen"
+                src="https://www.youtube.com/embed/2vIeNWYCs3Q?start=0&mute=1&autoplay=1&rel=0&controls=0&showinfo=0&fs=0&vq=hd1080" 
+                title="banner 1" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin"
+                allowfullscreen>
+                </iframe>
             </div>
         </>
     );
