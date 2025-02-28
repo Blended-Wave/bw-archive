e7602db9d8611eb5d9217c68bf346c0183f70dd7
KIM SEON WOO <sunwoo005@naver.com>
2025-03-01 04:32:57 +0900

    [front] style : Navbar, Footer 스타일 수정

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -3,12 +3,17 @@ import styles from '@/styles/Footer.module.css';
 export default function Footer() {
     return (
         <footer className={styles.footer_container}>
-            <img src="main_logo.svg" alt="logo" />
-            <p>
-                아티스트 문의 / For artist inquiries (blendedwave@gmail.com)/<br />
-                사이트 개발자 문의 / For developer inquiries (12191747@inha.edu)
-            </p>
-            <p>Copyright ©2023 Blended Wave. All rights reserved. </p>
+            <div className={styles.footer_box}>
+                <img src="main_logo.svg" alt="logo" />
+            </div>
+            <div className={styles.footer_center}>
+                <p>아티스트 문의 / For artist inquiries (blendedwave@gmail.com)</p>
+                <p>사이트 개발자 문의 / For developer inquiries (12191735@inha.edu)</p>
+            </div>
+            <div className={styles.footer_right}>
+                <p>Copyright ©2025 Blended Wave. All rights reserved.</p>
+                <p  className={styles.footer_login}><a href="/admin">Member Login</a></p> {/* 로그인 버튼을 푸터로 이동 */}
+            </div>
         </footer>
     )
 }
--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -25,10 +25,11 @@ export default function Navbar() {
                             INFO
                         </Link>
                     </li>
+                    {/* 로그인 로직은 추후 수정할 수도(디자인), 로직은 로그인 페이지로 넘어간 뒤, 로그인 정보 있을 땐 admin 페이지로 수정 */}
                     <li>
-                        <Link href="/admin">
-                            Login
-                        </Link>
+                        {/* <Link href="/admin">
+                            LOGIN
+                        </Link> */}
                     </li>
                 </ul>
             </div>
